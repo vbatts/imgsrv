d80427788d48ff7cf7dc14a93ee734f236e81af9
Vincent Batts <vbatts@redhat.com>
2013-05-09 10:01:38 -0400

    bumping version

--- version.go
+++ version.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 var (
 	MAJOR   int    = 1
-	MINOR   int    = 0
+	MINOR   int    = 1
 	TINY    int    = 0
 	VERSION string = fmt.Sprintf("%d.%d.%d", MAJOR, MINOR, TINY)
 )
