9cc9b88d264080966a8b778a216ab44de8812b4a
Vincent Batts <vbatts@hashbangbash.com>
2013-10-09 09:41:02 -0400

    thinking about how to do a stand alone web server

--- server/server.go
+++ server/server.go
@@ -1,13 +1,16 @@
 package server
 
 import (
-  "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
+	"github.com/vbatts/imgsrv/config"
 	"github.com/vbatts/imgsrv/dbutil"
 )
 
 type Web struct {
-  Router *mux.Router
-  Store  dbutil.Util
+	Router *mux.Router
+	Store  dbutil.Util
 }
 
-
+func New(c config.Config) *Web {
+	return &Web{}
+}
